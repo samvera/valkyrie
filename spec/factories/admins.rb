dd077c970bbee68a2f307afb2c0bb352aa20445e
Trey Pendragon <tpendragon@princeton.edu>
2016-09-29 13:56:30 -0700

    Add button to create a work

---
+++ spec/factories/admins.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :admin, parent: :user do
+    sequence(:email) { |_n| "email-#{srand}@test.com" }
+    password 'a password'
+    password_confirmation 'a password'
+    after(:build) do |user|
+      def user.groups
+        ['admin']
+      end
+    end
+  end
+end
---
+++ spec/features/admin_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "Administrative home page" do
+  background do
+    login_as FactoryGirl.create(:admin)
+  end
+  scenario "displaying the front page" do
+    visit root_path
+    expect(page).to have_link "New Book", href: new_book_path
+  end
+end
