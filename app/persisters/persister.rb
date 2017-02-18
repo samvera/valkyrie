15442790e8c92a7dc68c61368c946a0a9da651f1
Trey Pendragon <tpendragon@princeton.edu>
2017-02-17 18:30:30 -0800

    Allow for multiple persisters.

--- app/persisters/persister.rb
+++ app/persisters/persister.rb
@@ -1,8 +1,21 @@
 class Persister
   class_attribute :adapter
-  self.adapter = Valkyrie::Persistence::Postgres::Persister
+  self.adapter = Valkyrie::Persistence::Postgres
   class << self
-    delegate :save, :persister, to: :adapter
+    delegate :save, :persister, to: :default_adapter
+
+    def default_adapter
+      self.new(adapter: adapter)
+    end
+  end
+
+  delegate :save, :persister, to: :adapted_persister
+  def initialize(adapter:)
+    @adapter = adapter
+  end
+
+  def adapted_persister
+    "#{adapter}::Persister".constantize
   end
 
   class ObjectNotFoundError < StandardError
