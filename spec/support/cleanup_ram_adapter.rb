150f50ba02c792a8cc614940c6257ea21fc4c38f
Trey Pendragon <tpendragon@princeton.edu>
2017-03-31 10:39:51 -0700

    Make rubocop happy.

--- spec/support/cleanup_ram_adapter.rb
+++ spec/support/cleanup_ram_adapter.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
   config.before(:each) do
-    Valkyrie::Adapter.adapters.values.select do |adapter|
-      adapter.is_a?(Valkyrie::Persistence::Memory::Adapter)
-    end.each do |adapter|
+    Valkyrie::Adapter.adapters.values.each do |adapter|
+      next unless adapter.is_a?(Valkyrie::Persistence::Memory::Adapter)
       adapter.cache = {}
     end
   end
