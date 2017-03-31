74f4bf96fff7584621bf1074118676ee8b931813
Carrick Rogers <carrickr@umich.edu>
2017-03-31 14:24:59 -0400

    use models over book in find_members

--- app/persistence/valkyrie/persistence/fedora/query_service.rb
+++ app/persistence/valkyrie/persistence/fedora/query_service.rb
@@ -10,8 +10,8 @@ module Valkyrie::Persistence::Fedora
         Valkyrie::Persistence::Fedora::Queries::FindByIdQuery.new(id).run
       end
 
-      def find_members(book)
-        Valkyrie::Persistence::Fedora::Queries::FindMembersQuery.new(book).run
+      def find_members(model)
+        Valkyrie::Persistence::Fedora::Queries::FindMembersQuery.new(model).run
       end
     end
   end
