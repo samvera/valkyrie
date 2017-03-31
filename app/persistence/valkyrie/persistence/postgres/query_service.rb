677599b32b82d9d9da4db0d8d0f22662e9e81386
Carrick Rogers <carrickr@umich.edu>
2017-03-31 14:24:59 -0400

    use models over book in find_members

--- app/persistence/valkyrie/persistence/postgres/query_service.rb
+++ app/persistence/valkyrie/persistence/postgres/query_service.rb
@@ -10,8 +10,8 @@ module Valkyrie::Persistence::Postgres
         Valkyrie::Persistence::Postgres::Queries::FindByIdQuery.new(id).run
       end
 
-      def find_members(book)
-        Valkyrie::Persistence::Postgres::Queries::FindMembersQuery.new(book).run
+      def find_members(model)
+        Valkyrie::Persistence::Postgres::Queries::FindMembersQuery.new(model).run
       end
     end
   end
