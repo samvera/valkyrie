0e1b9b6fb84a39ce394daa54f335344e0358c994
Trey Pendragon <tpendragon@princeton.edu>
2017-05-24 12:31:06 -0700

    Move active_model around.

--- app/models/valkyrie/model.rb
+++ app/models/valkyrie/model.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 module Valkyrie
-  def config
-    Config.new(
-      YAML.safe_load(ERB.new(File.read(Rails.root.join("config", "valkyrie.yml"))).result)[Rails.env]
-    )
-  end
-
-  class Config < OpenStruct
-    def adapter
-      Valkyrie::Adapter.find(super.to_sym)
-    end
-
-    def storage_adapter
-      Valkyrie::FileRepository.find(super.to_sym)
-    end
-  end
-
-  module_function :config
   class Model < Dry::Struct
     include Draper::Decoratable
     constructor_type :schema
