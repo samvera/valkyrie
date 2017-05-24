cb85c89b3ef7a9920481d7a61ac83f0ad5b429bf
Chris Syversen <csyversen@gmail.com>
2017-05-24 15:44:19 -0500

    fixed rubocop offenses

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124135846) do
-
+ActiveRecord::Schema.define(version: 20_170_124_135_846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -62,5 +62,4 @@ ActiveRecord::Schema.define(version: 20170124135846) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
