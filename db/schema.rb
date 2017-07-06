298b8db14463a32c2e6c9e8ea9027024193e0699
Justin Coyne <jcoyne@justincoyne.com>
2017-07-06 12:09:59 -0500

    Rubocop mangled the schema file. Restore its previous glory

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_531_004_548) do
+ActiveRecord::Schema.define(version: 20170531004548) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -62,4 +62,5 @@ ActiveRecord::Schema.define(version: 20_170_531_004_548) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
