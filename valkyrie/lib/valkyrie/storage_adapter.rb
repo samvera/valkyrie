5cf3fe932214f626621badc90082998df882e427
Justin Coyne <jcoyne@justincoyne.com>
2017-07-11 12:55:06 -0500

    Fedora storage adapter
    This is a naive adapter that puts the uploaded file in the objects
    container at a sub-uri of `/original` and adds the pcdm-use:originalFile
    type in the metadata.

--- valkyrie/lib/valkyrie/storage_adapter.rb
+++ valkyrie/lib/valkyrie/storage_adapter.rb
@@ -36,6 +36,73 @@ module Valkyrie
       end.find_by(id: id)
     end
 
+    class Fedora
+      attr_reader :connection
+      PROTOCOL = 'fedora://'
+      def initialize(connection:)
+        @connection = connection
+      end
+
+      # @param id [Valkyrie::ID]
+      # @return [Boolean] true if this adapter can handle this type of identifer
+      def handles?(id:)
+        id.to_s.start_with?(PROTOCOL)
+      end
+
+      # Return the file associated with the given identifier
+      # @param id [Valkyrie::ID]
+      # @return [Valkyrie::StorageAdapter::File]
+      def find_by(id:)
+        File.new(id: id, io: response(id: id))
+      end
+
+      # @param file [IO]
+      # @param model [Valkyrie::Model]
+      # @return [Valkyrie::StorageAdapter::File]
+      def upload(file:, model:)
+        # TODO: this is a very naive aproach. Change to PCDM
+        identifier = model.id.to_uri + '/original'
+        ActiveFedora::File.new(identifier) do |af|
+          af.content = file
+          af.save!
+          af.metadata.set_value(:type, af.metadata.type + [::RDF::URI('http://pcdm.org/use#OriginalFile')])
+          af.metadata.save
+        end
+        find_by(id: Valkyrie::ID.new(identifier.to_s.sub(/^.+\/\//, PROTOCOL)))
+      end
+
+      class IOProxy
+        # @param response [Ldp::Resource::BinarySource]
+        def initialize(source)
+          @source = source
+        end
+        delegate :read, to: :io
+
+        # There is no streaming support in faraday (https://github.com/lostisland/faraday/pull/604)
+        # @return [StringIO]
+        def io
+          @io ||= StringIO.new(@source.get.response.body)
+        end
+      end
+      private_constant :IOProxy
+
+      private
+
+        # @return [IOProxy]
+        def response(id:)
+          af_file = ActiveFedora::File.new(active_fedora_identifier(id: id))
+          IOProxy.new(af_file.ldp_source)
+        end
+
+        # Translate the Valkrie ID into a URL for the fedora file
+        # @return [RDF::URI]
+        def active_fedora_identifier(id:)
+          scheme = URI(ActiveFedora.config.credentials[:url]).scheme
+          identifier = id.to_s.sub(PROTOCOL, "#{scheme}://")
+          RDF::URI(identifier)
+        end
+    end
+
     class Disk
       attr_reader :base_path
       def initialize(base_path:)
---
+++ valkyrie/spec/valkyrie/storage_adapter/fedora_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require 'valkyrie/specs/shared_specs'
+require 'active_fedora/cleaner'
+include ActionDispatch::TestProcess
+
+RSpec.describe Valkyrie::StorageAdapter::Fedora do
+  before(:all) do
+    # Start from a clean fedora
+    ActiveFedora::Cleaner.clean!
+  end
+  it_behaves_like "a Valkyrie::StorageAdapter"
+  let(:storage_adapter) { described_class.new(connection: ActiveFedora.fedora.connection) }
+  let(:file) { fixture_file_upload('files/example.tif', 'image/tiff') }
+end
