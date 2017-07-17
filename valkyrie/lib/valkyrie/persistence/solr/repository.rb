261aded3402d18b6cb953bbb6f42d3a6fcf99199
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-07-17 19:54:49 +0100

    Raise IllegalOperation in Solr persister

--- valkyrie/lib/valkyrie/persistence/solr/repository.rb
+++ valkyrie/lib/valkyrie/persistence/solr/repository.rb
@@ -10,7 +10,12 @@ module Valkyrie::Persistence::Solr
 
     def persist
       documents = models.map do |model|
-        generate_id(model) if model.id.blank?
+        if model.id.blank?
+          generate_id(model)
+        elsif model.created_at.blank? && model.id.present?
+          raise Valkyrie::Persistence::IllegalOperation, "Attempting to recreate existing resource: `#{model.id}'" if exists?(model)
+        end
+
         solr_document(model)
       end
       connection.add documents, params: { softCommit: true }
@@ -24,6 +29,13 @@ module Valkyrie::Persistence::Solr
       models
     end
 
+    def exists?(model)
+      query_service.find_by(id: model.id)
+      true
+    rescue ::Valkyrie::Persistence::ObjectNotFoundError
+      false
+    end
+
     def solr_document(model)
       resource_factory.from_model(model).to_h
     end
@@ -31,6 +43,10 @@ module Valkyrie::Persistence::Solr
     def generate_id(model)
       Valkyrie.logger.warn "The Solr adapter is not meant to persist new resources, but is now generating an ID."
       model.id = SecureRandom.uuid
+    end
+
+    def query_service
+      Valkyrie::Persistence::Solr::QueryService.new(connection: connection, resource_factory: resource_factory)
     end
   end
 end
