01d3a2f9a6d869f10813468e82dbbe375876d203
Trey Pendragon <tpendragon@princeton.edu>
2017-09-01 13:46:56 -0700

    Configurable bucketed path.

--- valkyrie/lib/valkyrie/storage/disk.rb
+++ valkyrie/lib/valkyrie/storage/disk.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 module Valkyrie::Storage
   class Disk
-    attr_reader :base_path
-    def initialize(base_path:)
+    attr_reader :base_path, :path_generator
+    def initialize(base_path:, path_generator: BucketedStorage)
       @base_path = Pathname.new(base_path.to_s)
+      @path_generator = path_generator.new(base_path: base_path)
     end
 
     # @param file [IO]
     # @param resource [Valkyrie::Resource]
     # @return [Valkyrie::StorageAdapter::File]
     def upload(file:, resource: nil)
-      new_path = base_path.join(resource.try(:id).to_s, file.original_filename)
+      new_path = path_generator.generate(resource: resource, file: file)
       FileUtils.mkdir_p(new_path.parent)
       FileUtils.mv(file.path, new_path)
       find_by(id: Valkyrie::ID.new("disk://#{new_path}"))
@@ -32,6 +33,22 @@ module Valkyrie::Storage
     def find_by(id:)
       return unless handles?(id: id)
       Valkyrie::StorageAdapter::File.new(id: Valkyrie::ID.new(id.to_s), io: ::File.open(file_path(id), 'rb'))
+    end
+
+    class BucketedStorage
+      attr_reader :base_path
+      def initialize(base_path:)
+        @base_path = base_path
+      end
+
+      def generate(resource:, file:)
+        Pathname.new(base_path).join(*bucketed_path(resource.id)).join(file.original_filename)
+      end
+
+      def bucketed_path(id)
+        cleaned_id = id.to_s.delete("-")
+        cleaned_id[0..5].chars.each_slice(2).map(&:join) + [cleaned_id]
+      end
     end
   end
 end
