c0d07ded86c46795b674d00ad3785b2f5a0bc3d1
Trey Pendragon <tpendragon@princeton.edu>
2017-09-05 13:44:38 -0700

    Move casting logic in Postgres.

--- valkyrie/lib/valkyrie/persistence/postgres/resource_factory.rb
+++ valkyrie/lib/valkyrie/persistence/postgres/resource_factory.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'valkyrie/persistence/postgres/orm_converter'
+require 'valkyrie/persistence/postgres/resource_converter'
 module Valkyrie::Persistence::Postgres
   class ResourceFactory
     class << self
@@ -14,10 +15,11 @@ module Valkyrie::Persistence::Postgres
       # @return [Valkyrie::Persistence::Postgres::ORM::Resource] ActiveRecord
       #   resource for the Valkyrie resource.
       def from_resource(resource)
-        ::Valkyrie::Persistence::Postgres::ORM::Resource.find_or_initialize_by(id: resource.id.to_s).tap do |orm_object|
-          orm_object.internal_resource = resource.internal_resource
-          orm_object.metadata.merge!(resource.attributes.except(:id, :internal_resource, :created_at, :updated_at))
-        end
+        ::Valkyrie::Persistence::Postgres::ResourceConverter.new(resource, resource_factory: self).convert!
+        # ::Valkyrie::Persistence::Postgres::ORM::Resource.find_or_initialize_by(id: resource.id.to_s).tap do |orm_object|
+        #   orm_object.internal_resource = resource.internal_resource
+        #   orm_object.metadata.merge!(resource.attributes.except(:id, :internal_resource, :created_at, :updated_at))
+        # end
       end
 
       def orm_class
