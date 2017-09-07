5d86c5ef2e04243c2b41390465ba1681e1c6f127
Adam Wead <amsterdamos@gmail.com>
2017-09-07 10:58:44 -0400

    Add test for dashboard controller user query

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
   describe "GET #index" do
+    let(:user_query) { ["collection", "work1", "work2"] }
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+      expect(assigns(:documents)).to eq(user_query)
     end
   end
 end
