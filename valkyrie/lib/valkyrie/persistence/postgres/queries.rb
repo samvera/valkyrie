f4453f4cce68b9b9004ec1855916319c8ddb8b5b
Trey Pendragon <tpendragon@princeton.edu>
2017-09-08 14:55:50 -0700

    Add a method for running a query to Postgres QS.
    I'm happy to have this turned down, but it gets rid of a lot of
    boilerplate.

--- valkyrie/lib/valkyrie/persistence/postgres/queries.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-require 'valkyrie/persistence/postgres/queries/find_inverse_references_query'
-require 'valkyrie/persistence/postgres/queries/find_members_query'
-require 'valkyrie/persistence/postgres/queries/find_references_query'
-module Valkyrie::Persistence::Postgres
-  module Queries
-  end
-end
