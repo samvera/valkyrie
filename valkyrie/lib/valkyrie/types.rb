7f8fef0ac5bbde4a5cbd9dc02410e5681e708e7e
Justin Coyne <jcoyne@justincoyne.com>
2017-10-27 13:11:39 -0500

    Cast ID types to single valued

--- valkyrie/lib/valkyrie/types.rb
+++ valkyrie/lib/valkyrie/types.rb
@@ -4,12 +4,19 @@ module Valkyrie
   # Namespace for Dry::Types types.
   module Types
     include Dry::Types.module
+
     # Valkyrie::ID
     ID = Dry::Types::Definition
          .new(Valkyrie::ID)
          .constructor do |input|
-      Valkyrie::ID.new(input)
+      if input.respond_to?(:each)
+        # Solr::ORMConverter tries to pass an array of Valkyrie::IDs
+        Valkyrie::ID.new(input.first)
+      else
+        Valkyrie::ID.new(input)
+      end
     end
+
     # Valkyrie::URI
     URI = Dry::Types::Definition
           .new(RDF::URI)
@@ -20,6 +27,7 @@ module Valkyrie
         input
       end
     end
+
     # Used for casting {Valkyrie::Resources} if possible.
     Anything = Valkyrie::Types::Any.constructor do |value|
       if value.respond_to?(:fetch) && value.fetch(:internal_resource, nil)
@@ -28,6 +36,7 @@ module Valkyrie
         value
       end
     end
+
     # Represents an array of unique values.
     Set = Valkyrie::Types::Coercible::Array.constructor do |value|
       value.select(&:present?).uniq.map do |val|
