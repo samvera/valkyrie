dd485e92d88cc32329e6a96c974186de808f0fb7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-12-13 14:04:27 -0500

    Exposing methods to reduce upstream chaining
    Prior to this commit, the following was used to retrieve a `persister`
    or a `query_service`:
    
    ```ruby
    Valkyrie::MetadataAdapter.find(:indexing_persister).query_service
    ```
    
    With the following change, we can now use:
    
    ```ruby
    Valkyrie::MetadataAdapter.find_query_service_for(:indexing_persister)
    ```
    
    In removing the method chaining, any upstream dependency inversion
    becomes easier to implement and utilize.
    
    Responding to https://github.com/samvera/hyrax/pull/2358
    https://github.com/samvera/hyrax/pull/2358/files/b50c05a38c1dbe511fa46016db50b68373142014#diff-21e2261d854a7e8fb6ea96ec414bc7d8R47

--- lib/valkyrie/metadata_adapter.rb
+++ lib/valkyrie/metadata_adapter.rb
@@ -14,10 +14,34 @@ module Valkyrie
       # Find an adapter by its short name.
       # @param short_name [Symbol]
       # @return [#persister,#query_service]
+      # @raise RuntimeError when the given short_name is not found amongst the registered adapters
       def find(short_name)
         symbolized_key = short_name.to_sym
+        # TODO: Instead of RuntimeError could we simply perform `adapters.fetch(symbolized_key)` this would raise a KeyError instead
         return adapters[symbolized_key] if adapters.key?(symbolized_key)
         raise "Unable to find unregistered adapter `#{short_name}'"
+      end
+
+      # @api public
+      # @since 0.1.0
+      # Find the persister registered under the given short-name
+      #
+      # @param short_name [Symbol]
+      # @return [Object] an object that behaves like "a Valkyrie::Persister"
+      # @see GEM_ROOT/lib/valkyrie/specs/shared_specs/persister.rb
+      def find_persister_for(short_name)
+        find(short_name).persister
+      end
+
+      # @api public
+      # @since 0.1.0
+      # Find the query service registered under the given short-name
+      #
+      # @param short_name [Symbol]
+      # @return [Object] an object that behaves like "a Valkyrie query provider"
+      # @see GEM_ROOT/lib/valkyrie/specs/shared_specs/queries.rb
+      def find_query_service_for(short_name)
+        find(short_name).query_service
       end
     end
   end
