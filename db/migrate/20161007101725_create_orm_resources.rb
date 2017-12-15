85060cf4ed01e15ef8bb93f1419e2b94d374498e
Justin Gondron <jgondron@nd.edu>
2017-12-15 16:39:31 -0500

    Hoping this appeases the cops

--- db/migrate/20161007101725_create_orm_resources.rb
+++ db/migrate/20161007101725_create_orm_resources.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 class CreateOrmResources < ActiveRecord::Migration[5.0]
-  def change
-    options = if ENV["VALKYRIE_ID_TYPE"] == "string"
+  def options
+    if ENV["VALKYRIE_ID_TYPE"] == "string"
       { id: :text, default: -> { '(uuid_generate_v4())::text' } }
     else
       { id: :uuid }
     end
+  end
+
+  def change
     create_table :orm_resources, **options do |t|
       t.jsonb :metadata, null: false, default: {}
       t.timestamps
