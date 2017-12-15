e6df0cbf3bdd72cd06c3b6d643ea453b1ec39e1e
Justin Gondron <jgondron@nd.edu>
2017-12-15 16:16:47 -0500

    - Separated the options from the create table for readability.

--- db/migrate/20161007101725_create_orm_resources.rb
+++ db/migrate/20161007101725_create_orm_resources.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 class CreateOrmResources < ActiveRecord::Migration[5.0]
   def change
-    if ENV["VALKYRIE_ID_TYPE"] == "string"
-      create_table :orm_resources, id: :text, default: -> { '(uuid_generate_v4())::text' } do |t|
-        t.jsonb :metadata, null: false, default: {}
-        t.timestamps
-      end
+    options = if ENV["VALKYRIE_ID_TYPE"] == "string"
+      { id: :text, default: -> { '(uuid_generate_v4())::text' } }
     else
-      create_table :orm_resources, id: :uuid do |t|
-        t.jsonb :metadata, null: false, default: {}
-        t.timestamps
-      end
+      { id: :uuid }
+    end
+    create_table :orm_resources, **options do |t|
+      t.jsonb :metadata, null: false, default: {}
+      t.timestamps
     end
     add_index :orm_resources, :metadata, using: :gin
   end
