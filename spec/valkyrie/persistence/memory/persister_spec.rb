4695f70a4761d953cc0f3a3758644b17e9886c2a
Carolyn Cole <cam156@psu.edu>
2018-08-03 10:07:57 -0400

    Adding optimistic locking to the memory persister fixes #455
    Co-authored-by: Mike Tribone <mtribone@psu.edu>
    Co-authored-by: Anna Headley <anna.headley@gmail.com>
    Co-authored-by: Adam Wead <amsterdamos@gmail.com>

--- spec/valkyrie/persistence/memory/persister_spec.rb
+++ spec/valkyrie/persistence/memory/persister_spec.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 require 'spec_helper'
 require 'valkyrie/specs/shared_specs'
+require 'valkyrie/specs/shared_specs/locking_persister'
 
 RSpec.describe Valkyrie::Persistence::Memory::Persister do
   let(:adapter) { Valkyrie::Persistence::Memory::MetadataAdapter.new }
   let(:query_service) { adapter.query_service }
   let(:persister) { adapter.persister }
   it_behaves_like "a Valkyrie::Persister"
+  it_behaves_like "a Valkyrie locking persister"
 end
