29599e7c8f76070cfbefc3d04c0d5ee8d301a5d3
GriffinJ <jrgriffiniii@gmail.com>
2018-08-07 09:44:15 -0400

    Adds the shared spec for persisting ordered properties; Ensures that the in-memory adapter persists ordered properties
    Co-authored-by: Trey Pendragon <tpendragon@princeton.edu>

--- lib/valkyrie/specs/shared_specs/supports_ordered_persisting.rb
+++ lib/valkyrie/specs/shared_specs/supports_ordered_persisting.rb
@@ -11,15 +11,15 @@ RSpec.shared_examples 'it supports persisting ordered properties' do
   after do
     Object.send(:remove_const, :CustomResource)
   end
-
   subject { persister }
   let(:resource_class) { CustomResource }
   let(:resource) { resource_class.new }
-
   it "saves ordered properties and returns them in the appropriate order" do
     resource.authors = ["a", "b", "a"]
     output = persister.save(resource: resource)
-
     expect(output.authors).to eq ["a", "b", "a"]
+
+    reloaded = query_service.find_by(id: output.id)
+    expect(reloaded.authors).to eq ["a", "b", "a"]
   end
 end
