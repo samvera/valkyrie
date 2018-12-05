c0313696fc39f21c86ef025a8779648ef473fda2
Esmé Cowles <escowles@ticklefish.org>
2018-12-04 20:45:42 -0500

    Fixing UTC/localtime mismatch bug

--- spec/valkyrie/persistence/solr/persister_spec.rb
+++ spec/valkyrie/persistence/solr/persister_spec.rb
@@ -64,8 +64,8 @@ RSpec.describe Valkyrie::Persistence::Solr::Persister do
     let(:resource_class) { CustomResource }
 
     it "Returns a string when DateTime conversion fails" do
-      time1 = DateTime.current
-      time2 = Time.current.in_time_zone
+      time1 = DateTime.current.utc
+      time2 = Time.current.utc
       allow(DateTime).to receive(:iso8601).and_raise StandardError.new("bogus exception")
       book = persister.save(resource: resource_class.new(title: [time1], author: [time2]))
 
