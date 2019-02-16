3e52207cfed6ff9d3a8117b884939f0dbe604ee8
Trey Pendragon <tpendragon@princeton.edu>
2019-02-15 17:06:39 -0800

    Remove RSolr dependency.
    Closes #675

--- lib/valkyrie/persistence/solr.rb
+++ lib/valkyrie/persistence/solr.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
+# :nocov:
+begin
+  gem 'rsolr'
+rescue Gem::LoadError => e
+  raise Gem::LoadError,
+        "You are using the Solr adapter without installing the #{e.name} gem.  "\
+        "Add `gem '#{e.name}'` to your Gemfile."
+end
+# :nocov:
 module Valkyrie::Persistence
   # Implements the DataMapper Pattern to store metadata into Solr
   module Solr
-    # Deprecation to allow us to make rsolr an optional dependency
-    path = Bundler.definition.gemfiles.first
-    matches = File.readlines(path).select { |l| l =~ /gem ['"]rsolr\b/ }
-    if matches.empty?
-      warn "[DEPRECATION] rsolr will not be included as a dependency in Valkyrie's gemspec as of the next major release. Please add the gem directly to your Gemfile if you use a solr adapter."
-    end
     require 'valkyrie/persistence/postgres/metadata_adapter'
     require 'valkyrie/persistence/solr/metadata_adapter'
     require 'valkyrie/persistence/solr/composite_indexer'
