b92934a06b8f67bf397034713397dcf86e31e3fd
Tom Johnson <tomjohnson@ucsb.edu>
2019-04-18 12:38:43 -0700

    Remove Solr adapter's dependency on Postgresql
    This require appears to just be in error. It was readded to support a
    deprecation, which is now removed.

--- lib/valkyrie/persistence/solr.rb
+++ lib/valkyrie/persistence/solr.rb
@@ -11,7 +11,6 @@ end
 module Valkyrie::Persistence
   # Implements the DataMapper Pattern to store metadata into Solr
   module Solr
-    require 'valkyrie/persistence/postgres/metadata_adapter'
     require 'valkyrie/persistence/solr/metadata_adapter'
     require 'valkyrie/persistence/solr/composite_indexer'
   end
