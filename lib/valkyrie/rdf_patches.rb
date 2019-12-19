3312fb387c6776839f02bf12ca0d70afa46d078d
Tom Johnson <tomjohnson@ucsb.edu>
2019-12-18 18:27:23 -0800

    Use `::` prefixed names for `JSON::LD` references
    `RDF::JSON` exists in some contexts, e.g. after `require `linkeddata`. When it
    does, this name fails to resolve. Using the anchor `::` ensures we look for
    `JSON::LD` in the top level namespace.

--- lib/valkyrie/rdf_patches.rb
+++ lib/valkyrie/rdf_patches.rb
@@ -6,12 +6,12 @@
 module RDF
   class Literal
     def as_json(*_args)
-      JSON::LD::API.fromRdf([RDF::Statement.new(RDF::URI(""), RDF::URI(""), self)])[0][""][0]
+      ::JSON::LD::API.fromRdf([RDF::Statement.new(RDF::URI(""), RDF::URI(""), self)])[0][""][0]
     end
   end
   class URI
     def as_json(*_args)
-      JSON::LD::API.fromRdf([RDF::Statement.new(RDF::URI(""), RDF::URI(""), self)])[0][""][0]
+      ::JSON::LD::API.fromRdf([RDF::Statement.new(RDF::URI(""), RDF::URI(""), self)])[0][""][0]
     end
   end
 end
