886204fb1a2e1b2d7f2a5c4f5998e81615273138
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-07-22 00:56:26 -0400

    Should be fixed. Array -> HashConfig -> Hash

--- lib/config/database_connection.rb
+++ lib/config/database_connection.rb
@@ -9,7 +9,7 @@ module DatabaseConnection
       ::ActiveRecord::Base.connection_pool.disconnect! if ::ActiveRecord::Base.connected?
       ::ActiveRecord::Base.configurations = YAML.safe_load(ERB.new(File.read("db/config.yml")).result, [], [], true) || {}
       # configs_for is replacing deprecated [] call on configurations - ternary to bridge rails versions
-      config = ::ActiveRecord::Base.configurations.respond_to?(:configs_for) ? ::ActiveRecord::Base.configurations.configs_for(env_name: env.to_s).first : ::ActiveRecord::Base.configurations[env.to_s]
+      config = ::ActiveRecord::Base.configurations.respond_to?(:configs_for) ? ::ActiveRecord::Base.configurations.configs_for(env_name: env.to_s).first.configuration_hash : ::ActiveRecord::Base.configurations[env.to_s]
       ::ActiveRecord::Base.establish_connection(config)
     end
   end
