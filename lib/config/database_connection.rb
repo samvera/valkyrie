b085cfa6c03c69cc3e4558cd5c80120252090a81
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-07-22 02:07:55 -0400

    Return to ternary, but with shorter line

--- lib/config/database_connection.rb
+++ lib/config/database_connection.rb
@@ -8,10 +8,10 @@ module DatabaseConnection
     ActiveSupport.on_load(:active_record) do
       ::ActiveRecord::Base.connection_pool.disconnect! if ::ActiveRecord::Base.connected?
       ::ActiveRecord::Base.configurations = YAML.safe_load(ERB.new(File.read("db/config.yml")).result, [], [], true) || {}
-      # configs_for is replacing deprecated [] call on configurations - ternary to bridge rails versions
-      # using Safe Navigation Operator - should be fine as we only test for > ruby 2.3
-      config = ::ActiveRecord::Base.configurations&.configs_for(env_name: env.to_s)&.first&.configuration_hash
-      config = ::ActiveRecord::Base.configurations[env.to_s] if config.nil?
+      # configs_for is replacing deprecated (as of Rails 6.2) [] call on configurations
+      # ActiveRecord::DatabaseConfigurations or Hash ternary
+      precursor = ::ActiveRecord::Base.configurations
+      config = precursor.is_a?(ActiveRecord::DatabaseConfigurations) ? precursor.configs_for(env_name: env.to_s).first.configuration_hash : precursor[env.to_s]
       ::ActiveRecord::Base.establish_connection(config)
     end
   end
