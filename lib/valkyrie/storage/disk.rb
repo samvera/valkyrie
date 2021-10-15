0b1a9ec85d266a49fe27557cd27b123fa2a9e503
Trey Pendragon <tpendragon@princeton.edu>
2021-10-14 18:21:24 -0700

    Don't close if you can't.

--- lib/valkyrie/storage/disk.rb
+++ lib/valkyrie/storage/disk.rb
@@ -18,7 +18,7 @@ module Valkyrie::Storage
       new_path = path_generator.generate(resource: resource, file: file, original_filename: original_filename)
       FileUtils.mkdir_p(new_path.parent)
       file_mover.call(file.path, new_path)
-      file.close
+      file.try(:close)
       find_by(id: Valkyrie::ID.new("disk://#{new_path}"))
     end
 
