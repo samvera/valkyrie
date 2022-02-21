8dc349ff7e4c6a2f4de036125f4680dbafe45125
Carla Galarza <cgalarza@upenn.edu>
2022-02-21 17:35:11 -0500

    Attempting to fix specs

--- spec/valkyrie/persistence/postgres/metadata_adapter_spec.rb
+++ spec/valkyrie/persistence/postgres/metadata_adapter_spec.rb
@@ -7,10 +7,24 @@ RSpec.describe Valkyrie::Persistence::Postgres::MetadataAdapter do
   it_behaves_like "a Valkyrie::MetadataAdapter"
 
   describe "#id" do
+    let(:db_config) { adapter.send(:connection_configuration) }
+
     it "creates an md5 hash from the host and database name" do
-      to_hash = "#{ActiveRecord::Base.connection_config['host']}:#{ActiveRecord::Base.connection_config['database']}"
+      to_hash = "#{db_config[:host]}:#{db_config[:database]}"
       expected = Digest::MD5.hexdigest to_hash
       expect(adapter.id.to_s).to eq expected
+    end
+  end
+
+  describe "#connection_configuration" do
+    let(:config) { adapter.send(:connection_configuration) }
+
+    it 'returns hash with :host key' do
+      expect(config[:host]).not_to be_nil
+    end
+
+    it 'returns hash with :database key' do
+      expect(config[:database]).not_to be_nil
     end
   end
 end
